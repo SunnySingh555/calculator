731e576eefeb715924f7b5ac6c6b23086fb4f223
SunnySingh555 <78091432+SunnySingh555@users.noreply.github.com>
2021-01-28 20:06:56 +0530

    Added comment to explain addition

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -9,9 +9,9 @@ exports.calculate = function(req, res) {
     res.status(400);
     res.json({ error: err.message });
   });
-
+// Plus sign in required to avoid the concatenation
   var operations = {
-    'add':      function(a,b) { return a + b },
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
